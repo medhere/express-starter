4f88d9201e497fa9f9c63ec1433985f8c4459127
medhere <edhere.michael@gmail.com>
2022-08-26 09:16:41 +0100

    updated dbwithperms

--- dbwithperms.sql
+++ dbwithperms.sql
@@ -1,3 +1,9 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.13 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.2.0.6213
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET NAMES utf8 */;
@@ -7,46 +13,61 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Dumping database structure for manager
-CREATE DATABASE IF NOT EXISTS `manager` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin */;
-USE `manager`;
+-- Dumping database structure for estate
 
--- Dumping structure for table manager.can_give_permissions
+-- Dumping structure for table estate.can_give_permissions
 CREATE TABLE IF NOT EXISTS `can_give_permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   `perm_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `perm_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `perm_value` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC;
 
--- Data exporting was unselected.
+-- Dumping data for table estate.can_give_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `can_give_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `can_give_permissions` ENABLE KEYS */;
 
--- Dumping structure for table manager.users
+-- Dumping structure for table estate.permissions
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `value` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table estate.permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+
+-- Dumping structure for table estate.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(50) COLLATE utf8mb4_bin NOT NULL DEFAULT '0',
   `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `pass` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `fullname` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `supervisor_id` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `userpass` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `supervisor_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `email` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `updated_at` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Data exporting was unselected.
+-- Dumping data for table estate.users: ~0 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
--- Dumping structure for table manager.user_permissions
+-- Dumping structure for table estate.user_permissions
 CREATE TABLE IF NOT EXISTS `user_permissions` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `perm_id` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
-  `perm_type` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
-  `perm_value` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `user_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
+  `perm_id` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
--- Data exporting was unselected.
+-- Dumping data for table estate.user_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_permissions` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
